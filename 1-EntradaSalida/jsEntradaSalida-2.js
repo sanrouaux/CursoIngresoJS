83351254f9c96296fd8b575aac6f4a36ad869b4d
sanrouaux <40763525+sanrouaux@users.noreply.github.com>
2018-07-02 12:06:26 -0300

    Ejercicios 1 2 3

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,11 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
+	
+	var nombre;
+	nombre=prompt("Ingrese su nombre", "Nombre");
+	alert("Su nombre es: " + nombre);
 	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,16 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*
+	alert("Ejercicio 3");
+	console.log("Mensaje de consola");
+	*/
+
+	var nombre;
+	nombre=document.getElementById('elNombre').value;
+	alert(nombre);
+
 }
 
 
