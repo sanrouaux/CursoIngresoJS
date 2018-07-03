f4160837024001482669d31a150e250533aba939
sanrouaux <40763525+sanrouaux@users.noreply.github.com>
2018-07-03 00:10:59 -0300

    Ejercicios 1, 2 y 3 de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
 
+	var precio;
+	var porcentajeDescuento;
+	var precioFinal;
+
+	precio=prompt("Introduce el precio");	
+	porcentajeDescuento=prompt("Introduce el porcentaje de descuento");
+
+	precioFinal=parseInt(precio)-(parseInt(precio)*parseInt(porcentajeDescuento)/100);
+
+	alert(precioFinal);
+
+	document.
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
-
+	var ancho;
+	var largo;
+	var perimetro;
+	ancho=prompt("Introduce el ancho del rectangulo");
+	largo=prompt("Introduce el largo del rectangulo");
+	perimetro=2*parseInt(ancho)+2*parseInt(largo);
+	alert(perimetro);
 }
