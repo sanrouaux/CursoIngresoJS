da84c7714ed7830af0c19dc892660ddaa8f07247
sanrouaux <sanrouaux@gmail.com>
2018-07-10 12:28:30 -0300

    Clase del 10/07/2018

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,18 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var nota;
+
+	nota = Math.floor(Math.random()*(11-0))+0;
+
+	if(nota > 8){
+		alert("EXCELENTE");
+	} else {
+		if(nota >= 4){
+			alert("APROBÓ");
+		} else {
+			alert("Vamos, la próxima se puede");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
