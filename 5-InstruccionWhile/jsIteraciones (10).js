7f1d7f9d9466be50d982e0e767c262ff452a969a
sanrouaux <40763525+sanrouaux@users.noreply.github.com>
2018-07-24 11:05:37 -0700

    Add files via upload
    Correcciones en casa 24-7

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,7 +1,6 @@
-function mostrar()
-{
+function mostrar(){
 
-	var contador=0;
+
 	var numero;
 	var suma_positivos=0;
 	var suma_negativos=0;
@@ -9,8 +8,8 @@ function mostrar()
 	var cantidad_negativos=0;
 	var cantidad_ceros=0;
 	var cantidad_pares=0;
-	var promedio_positivos=1;
-	var promedio_negativos=1;
+	var promedio_positivos=0;
+	var promedio_negativos=0;
 	var diferencia=0;
 	var mensaje;
 	
@@ -21,49 +20,52 @@ function mostrar()
 		numero=prompt("Ingrese un número");
 		numero=parseInt(numero);
 
-		while(isNan(numero){
-			numero=prompt("Error! Ingrese un número válido.");
+		while(isNaN(numero)){
+			numero = prompt("Error! Ingrese un número válido.");
 		}
-
-	
+		
+		
 		if(numero>0){
 			suma_positivos += numero;
 			cantidad_positivos++;
+			promedio_positivos = suma_positivos/cantidad_positivos;
 						
-		} else if(numero<0){
-
+		}else if(numero<0){
 			suma_negativos += numero;
 			cantidad_negativos++;
+			promedio_negativos = suma_negativos/cantidad_negativos;
 					
-		} else{
-
+		}else{
 			cantidad_ceros++;
 		}
-
 
 		if(numero%2==0 && numero!=0){
 			cantidad_pares++;
 		}
 
 
-		promedio_positivos = suma_positivos/cantidad_positivos;
-		promedio_negativos = suma_negativos/cantidad_negativos;
-			
-		
 		diferencia=cantidad_positivos-cantidad_negativos;
 		if(diferencia>0){
-			mensaje="Hay más positivos que negativos: " + diferencia;
+			mensaje="Ademas, hay " + diferencia + " positivos mas que negativos";
 		}else if(diferencia==0){
-			mensaje="Hay igual cantidad de negativos y de positivos";
+			mensaje="Ademas, hay igual cantidad de negativos que de positivos";
 		}else if(diferencia<0){
-			mensaje="Hay más negativo que positivos: " + diferencia*(-1);
+			mensaje="Ademas, hay " + diferencia*(-1) + " negativos mas que positivos";
 		}
 	
 	
 		respuesta=prompt("¿Desea ingresar otro número? si/no")
 	}
 
-	document.write(suma_positivos );
+	document.write("La suma de los negativos es: " + suma_negativos + "<br>" +
+				   "La suma de los positivos es: " + suma_positivos + "<br>" +
+				   "La cantidad de positivos es: " + cantidad_positivos + "<br>" + 
+				   "La cantidad de negativos es: " + cantidad_negativos + "<br>" +
+				   "La cantidad de ceros es: " + cantidad_ceros + "<br>" +
+				   "La cantidad de numeros pares es: " + cantidad_pares + "<br>" +
+				   "El promedio de positivos es: " + promedio_positivos + "<br>" +
+				   "El promedio de negativos es: " + promedio_negativos + "<br>" +					
+					mensaje);
 
 
 }//FIN DE LA FUNCIÓN
